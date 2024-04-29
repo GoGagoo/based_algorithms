f2d3c0089e64cdd32a98062716a9cc05dcb14c48
Gagik Antonyan <gagika57@gmail.com>
2024-04-29 13:11:34 +0500

    Fix Selection Sort Algorithm

--- 3_selection_sort.js
+++ 3_selection_sort.js
@@ -1,24 +1,24 @@
 // Сортировка выбором
 
-const array = [0, 3, 2, 5, 6, 7, 8, 1, 9, 4, 1, 2, 9, 6, 4, 1, 7, -1, -5, 23, 6, 2, 35, 6, 3, 33]
+const arr = [0,3,2,5,6,8,1,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32] // [0,1,1,2,3.......]
 let count = 0
 
-function selectionSort(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		let indexMin = i
-		for (let j = i + 1; j < arr.length; j++) {
-			if (arr[i] < arr[indexMin]) {
-				indexMin = j
-			}
-			count += 1
-		}
-		let tmp = arr[i]
-		arr[i] = arr[indexMin]
-		arr[indexMin] = tmp
-	}
-	return arr
+function selectionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let indexMin = i
+        for (let j = i+1; j < array.length; j++) {
+            if (array[j] < array[indexMin]) {
+                indexMin = j
+            }
+            count += 1
+        }
+        let tmp = array[i]
+        array[i] = array[indexMin]
+        array[indexMin] = tmp
+    }
+    return array
 }
 
-console.log(selectionSort(array))
-console.log(array.length)
+console.log(selectionSort(arr))
+console.log(arr.length) // O(n*n)
 console.log('count = ', count)
